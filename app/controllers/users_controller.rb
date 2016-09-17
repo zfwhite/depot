d6f130fef33bac441e6ab222824cbc695a903a33
Zachary White <zfwhite91@gmail.com>
2016-09-16 18:05:40 -0700

    Updating user now requires password authentication.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,14 +41,17 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to users_url,
-          notice: "User #{@user.name} was successfully updated." }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+    current_user = User.find(params[:id])
+    if current_user.try(:authenticate, params[:user][:old_password])
+      respond_to do |format|
+        if @user.update(user_params)
+          format.html { redirect_to users_url,
+            notice: "User #{@user.name} was successfully updated." }
+          format.json { render :show, status: :ok, location: @user }
+        else
+          format.html { render :edit }
+          format.json { render json: @user.errors, status: :unprocessable_entity }
+        end
       end
     end
   end
