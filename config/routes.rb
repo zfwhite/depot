ff57ef075320b771ca2b0f1bbb459281cbb6b3cf
Zachary White <zfwhite91@gmail.com>
2016-09-19 15:49:59 -0700

    Orders can now be placed en espanol.

--- config/routes.rb
+++ config/routes.rb
@@ -6,15 +6,17 @@ Rails.application.routes.draw do
     post 'login'    => :create
     delete 'logout' => :destroy
   end
-  
-  resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
-  root 'store#index', as: 'store_index'
 
+  resources :users
   resources :products do
     get :who_bought, on: :member
+  end
+
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store_index', via: :all
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
